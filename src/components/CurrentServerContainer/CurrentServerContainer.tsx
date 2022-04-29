1a886d4b7a33d2961953848fa547d632a6a2e1f3
brianwinward <89710625+brianwinward@users.noreply.github.com>
2022-04-29 11:26:13 -0400

    added a space

--- src/components/CurrentServerContainer/CurrentServerContainer.tsx
+++ src/components/CurrentServerContainer/CurrentServerContainer.tsx
@@ -6,7 +6,7 @@ function CurrentServerContainer() {
     return (
         <div className='w-full h-screen'>
             <NavBar />
-            <div className='h-[calc(100%-48px)] flex flex-row'>
+            <div className='h-[calc(100%-48px)] flex flex-row'> 
                 <ChatWindow />
                 <UserRail />
             </div>
